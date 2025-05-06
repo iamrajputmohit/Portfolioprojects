38b58c27a96634a75af4ec5b1009ab483c5cd8c2
iamrajputmohit <mohitrajputcro9@gmail.com>
2025-05-06 22:02:25 +0530

    Update Portfolio.sql

--- Portfolio.sql
+++ Portfolio.sql
@@ -1,67 +1,70 @@
-select	*
-from covid_deaths_synthetic
-order by 3,4
+-- DATASET
+SELECT *
+FROM ECOMMERCE_SALES
 
-select location,date,total_cases,new_cases,total_deaths
-from covid_deaths_synthetic
-order by 3,4
+-- TOTAL SALES BY COUNTRY 
+SELECT country, SUM(quantity * price) AS total_sales
+FROM ecommerce_sales
+GROUP BY country
+ORDER BY total_sales asc
 
+-- Top 5 customers by profit
+SELECT customer_name,sum(profit) as total_proft
+FROM ecommerce_sales
+GROUP BY customer_name
+order by total_proft asc
+LIMIT 5;
 
+-- Monthly sales trend
+SELECT date_format(order_date,'%y-%m') as month, SUM(quantity * price) AS total_sales
+FROM ecommerce_sales
+GROUP BY month
+ORDER BY month
 
+-- Top 3 product categories by revenue
+SELECT product_category, SUM(quantity * price) AS total_sales
+FROM ecommerce_sales
+GROUP BY product_category
+ORDER BY total_sales desc
+limit 3;
 
--- looking at total case vs total deaths 
--- shows the likelyhood of dieing if you are convicted, in a afghanistan 
-SELECT location,date,total_cases, total_deaths, (total_deaths / total_cases) * 100 AS 'death%'
-FROM covid_deaths_synthetic;
-order by 1,2
+-- Sales trend over years
+SELECT date_format(order_date,'%y') as YEAR, SUM(quantity * price) AS total_sales
+FROM ecommerce_sales
+GROUP BY YEAR
+ORDER BY YEAR;
 
+-- Discount impact on sales
+SELECT discount,COUNT(*) AS NO_ODERS, SUM(quantity * price) AS total_sales
+FROM ecommerce_sales
+GROUP BY Discount
+ORDER BY Discount;
 
+-- Average order value per country
+SELECT country, avg(quantity * price) AS AVG_sales
+FROM ecommerce_sales
+GROUP BY country
+ORDER BY AVG_sales;
 
--- looking at countries with most cases
-select location,max(total_cases) as highestinfectioncount 
-from covid_deaths_synthetic
-group by location
-order by highestinfectioncount desc 
+-- Best-selling products overall
+SELECT product_name, SUM(quantity * price) AS TOTAL_sales
+FROM ecommerce_sales
+GROUP BY product_name
+ORDER BY TOTAL_sales DESC
+LIMIT 10;
 
+-- Profit margin per category
+SELECT product_category, SUM(PROFIT)/SUM(PRICE)*100 AS MARGIN
+FROM ecommerce_sales
+GROUP BY product_category
+ORDER BY MARGIN desc
 
-
--- looking at countries with most deaths
-select location,max(total_deaths) as highestdeathcount 
-from covid_deaths_synthetic
-group by location
-order by highestdeathcount desc 
-
-
-
--- Global Nubmers
-SELECT 
-    date, 
-    SUM(new_cases) AS total_cases, 
-    SUM(new_deaths) AS total_deaths, 
-    (SUM(new_deaths) / SUM(new_cases)) * 100 AS death_percentage
-FROM covid_deaths_synthetic
-GROUP BY date
-ORDER BY 1, 2 DESC;
-
-
--- total cases vs total vaccination 
-SELECT 
-    cd.date, 
-    GROUP_CONCAT(DISTINCT cd.location) AS locations,
-    SUM(cd.new_cases) AS total_cases,
-    SUM(cv.total_vaccinations) AS total_vaccinations
-FROM coviddeaths.covid_deaths_synthetic cd 
-JOIN covidvaccination.covid_vaccination_80000 cv 
-    ON cd.location = cv.location 
-    AND cd.date = cv.date 
-GROUP BY cd.date
-ORDER BY cd.date;
-
-
-
-
-
-
-
-
-
+-- Year-over-year growth
+SELECT YEAR(ORDER_DATE) as YEAR,
+      SUM(quantity * price) AS total_sales,
+      LAG(SUM(quantity * price)) OVER (ORDER BY YEAR (ORDER_DATE)) AS LAST_YEAR_SALES,
+      (SUM(quantity * price) - LAG(SUM(quantity * price)) OVER (ORDER BY YEAR (ORDER_DATE))
+      / LAG(SUM(quantity * price)) OVER (ORDER BY YEAR (ORDER_DATE)))*100 AS GROWTH_PERCENTAGE
+FROM ecommerce_sales
+GROUP BY YEAR
+ORDER BY YEAR;
